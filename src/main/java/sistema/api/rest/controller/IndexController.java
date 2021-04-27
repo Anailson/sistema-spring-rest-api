b0e67a5bf0c6aafc9b2397c6ca60740fe2b45cea
anailson ribeiro <arsantos315@gmail.com>
2021-04-27 15:43:55 -0300

    Retornando usuário em JSON para a tela

--- src/main/java/sistema/api/rest/controller/IndexController.java
+++ src/main/java/sistema/api/rest/controller/IndexController.java
@@ -1,24 +1,47 @@
 package sistema.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import sistema.api.rest.model.Usuario;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
-	
-	/*SERVIÇO RESTFULL*/
+
+	/* SERVIÇO RESTFULL */
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam(value = "nome",required = true, defaultValue = "Nome não informado") String nome) {
+	public ResponseEntity<Usuario> init() {
 		
-		System.out.println("Param sendo recebido" + nome);
+		//PASSANDO VALORES PARA A MODEL USUARIO
+		Usuario usuario = new Usuario();
+		usuario.setId(3L);
+		usuario.setNome("anailson");
+		usuario.setLogin("teste@gmail.com");
+		usuario.setSenha("ffsfsf");
 		
-		return new ResponseEntity("Olá REST Spring API nome: " + nome, HttpStatus.OK);
-		//http://localhost:8080/usuario/?nome=teste  - PASSANDO O PARAM 
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(10L);
+		usuario2.setNome("teste");
+		usuario2.setLogin("javaspring@gmail.com");
+		usuario2.setSenha("4510cssd");
+		
+		//RETORNANDO EM UMA LISTA DE USUARIOS
+		List<Usuario> usuarios = new  ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
+
 	}
 
 }
