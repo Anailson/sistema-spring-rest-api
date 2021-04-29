d813db63d2302ea2fba85a57d083d02e54d0b0a3
anailson ribeiro <arsantos315@gmail.com>
2021-04-29 09:18:28 -0300

    DELETE - Pelo ID

--- src/main/java/sistema/api/rest/controller/IndexController.java
+++ src/main/java/sistema/api/rest/controller/IndexController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,7 +64,14 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 		
+	}
+	/*----------------DELETE-----------------*/
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete (@PathVariable("id") Long id) {
 		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
 	}
 	
 
