3dfc09a56e308dac262817ff1d96811f60655ec0
anailson ribeiro <arsantos315@gmail.com>
2021-05-06 11:39:25 -0300

    Spring Security - Configurando

---
+++ src/main/java/sistema/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,68 @@
+package sistema.api.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import sistema.api.rest.service.ImplementacaoUserDetailsService;
+
+//MAPEIA URL, ENDEREÇOS, AUTORIZA OU BLOQUEIA ACESSO A URL
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity  extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService; //chamando a classe
+	
+	
+	//CONFIGURA AS SOLICITAÇÕES DE ACESSO POR HTTPS
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//ATIVANDO A PROTAÇÃO CONTRA USUARIO QUE NÃO ESTÃO VALIDADOS POR TOKEN
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		
+		//ATIVANDO A PERMISSÃO DE ACESSO PAGINA INICIAL EX: SISTEMA.COM.BR/INDEX
+		.disable().authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()//PERMITE TODOS ACESSO
+		
+		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
+		
+		//URL DE LOGOUT - REDIRECIONAR APOS O USER DESLOGAR DO SISTEMA
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+		//MAPEIA A URL DE LOGOUT E INVALIDA O USUARIO
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		
+		
+		// Filtra requisições de login para autenticação
+	
+				
+		// Filtra demais requisições para verificar a presença do TOKEN JWT no HEADER HTTP
+		
+		
+	}
+	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		//SERVICE QUE IRA CONSULTA  O USUARIO NO BANCO DE DADOS
+		auth.userDetailsService(implementacaoUserDetailsService)
+		
+		//PADRÃO DE CODIFICAÇÃO DE SENHA
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+	}
+	
+	
+
+}
